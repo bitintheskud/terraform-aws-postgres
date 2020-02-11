be96f6f7c9ac257f6b0db01f316b9df842b47b84
Alban MUSSI <alban_mussi@uptimemanagers.fr>
2020-02-11 11:32:57 +0100

    feat: add replica
    Signed-off-by: Alban MUSSI <alban_mussi@uptimemanagers.fr>

--- main.tf
+++ main.tf
@@ -110,3 +110,17 @@ module "db" {
 
   apply_immediately = var.apply_immediately
 }
+
+resource "aws_db_instance" "replica" {
+  count                  = var.db["replica"]["enable"] ? 1 : 0
+  identifier             = "${local.name_identifier}-replica"
+  instance_class         = var.db["instance_class"]
+  replicate_source_db    = module.db.this_db_instance_id
+  vpc_security_group_ids = ["${aws_security_group.db_allow_all.id}"]
+  allocated_storage      = var.db["allocated_storage"]
+  max_allocated_storage  = var.db["max_allocated_storage"]
+  ca_cert_identifier     = var.db["ca_cert_identifier"]
+  publicly_accessible    = var.db["publicly_accessible"]
+  availability_zone = var.db["replica"]["availability_zone"]
+  final_snapshot_identifier = "${local.name_identifier}-replica-snapshot"
+}
