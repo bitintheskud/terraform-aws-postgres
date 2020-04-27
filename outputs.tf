7c3b79e80424f42e594dd175897249b1e53d52ba
Alban MUSSI <alban_mussi@uptimemanagers.fr>
2020-04-27 16:11:07 +0200

    update: add security group id output

--- outputs.tf
+++ outputs.tf
@@ -5,16 +5,21 @@ output "db_password" {
 }
 
 output "db_instance_address" {
-  value = module.db.this_db_instance_address
+  value       = module.db.this_db_instance_address
   description = "DB instance address"
 }
 
 output "db_instance_endpoint" {
-  value = module.db.this_db_instance_endpoint
+  value       = module.db.this_db_instance_endpoint
   description = "DB instance endpoint"
 }
 
 output "db_instance_username" {
-  value = module.db.this_db_instance_username
+  value       = module.db.this_db_instance_username
   description = "DB instance username"
+}
+
+output "security_group_id" {
+  value       = aws_security_group.db_allow_all.id
+  description = "Security group id used by the Postgres Instance"
 }
